3cf9f4a4974e00be13da33e510b92579638259fd
misshanya <mishanyadev@gmail.com>
2025-05-09 12:01:51 +0300

    chore: update flake.lock

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -2,17 +2,19 @@
   services.mako = {
     enable = true;
 
-    font = "Sans 12";
-    backgroundColor = "#2E3440";
-    textColor = "#FFFFFF";
-    borderColor = "#4C566A";
-    borderSize = 2;
-    borderRadius = 10;
-    anchor = "bottom-left";
-    width = 400;
-    height = 100;
-    padding = "10";
-    defaultTimeout = 5000;
-    layer = "overlay";
+    settings = {
+      font = "Sans 12";
+      backgroundColor = "#2E3440";
+      textColor = "#FFFFFF";
+      borderColor = "#4C566A";
+      borderSize = 2;
+      borderRadius = 10;
+      anchor = "bottom-left";
+      width = 400;
+      height = 100;
+      padding = "10";
+      defaultTimeout = 5000;
+      layer = "overlay";
+    };
   };
 }
