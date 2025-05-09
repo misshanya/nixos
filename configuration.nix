fde045061de10262cded20556d2272ead37302a6
misshanya <mishanyadev@gmail.com>
2025-05-09 12:02:54 +0300

    feat: enable tailscale

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,9 @@
       package = pkgs.librewolf;
   };
 
+  # Tailscale
+  services.tailscale.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
